04b342c440c4b72a526bc7b2b0fdda46bfe2603c
egor <rank2007@mail.ru>
2021-09-25 11:43:19 +0300

    commit

--- src/layout/technologicalPart/technologicalPart.tsx
+++ src/layout/technologicalPart/technologicalPart.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {makeStyles} from "@material-ui/core";
 import {ReactComponent as VerticalLine} from "../../assets/logo/verticalLine.svg";
+import {ReactComponent as ArrowDown} from "../../assets/logo/arrowDown2.svg";
 
 const useStyles = makeStyles((theme) => ({
         technological: {
@@ -38,8 +39,59 @@ const useStyles = makeStyles((theme) => ({
         boxText: {
             fontFamily: "Inglobal",
             fontSize: 24,
-            // lineHeight: 36
-        }
+        },
+        linesArea: {
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            marginBottom: 60
+        },
+        leftLine: {
+            transform: "rotate(30deg)",
+            padding: "0 200px"
+        },
+        rightLine: {
+            transform: "rotate(330deg)",
+            padding: "0 200px"
+        },
+        circleArea: {
+            display: "flex",
+            alignItems: "center",
+            marginBottom: 80
+        },
+        circle: {
+            width: 596,
+            height: 596,
+            borderRadius: "100%",
+            backgroundColor: "#075AB4",
+            boxShadow: "0px 4px 30px 0px rgba(7, 90, 180, 0.3)",
+            margin: "0 60px",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center"
+        },
+        circleTitle: {
+            fontFamily: "Inglobal",
+            fontSize: 36,
+            color: "white",
+            margin: "100px 0 30px",
+        },
+        circleSubTitle: {
+            fontFamily: "Inglobal",
+            fontSize: 24,
+            textAlign: "center",
+            color: "white",
+            marginTop: 30
+        },
+        subTitle: {
+            textAlign: "center",
+            fontFamily: "Inglobal",
+            fontSize: 36,
+            marginBottom: 100
+        },
+        underLine: {
+            borderBottom: "2px solid #DA442B"
+        },
     }),
 );
 
@@ -57,9 +109,28 @@ function TechnologicalPart() {
                         <span className={classes.boxText}>Система по розливу воды</span>
                     </div>
                 </div>
-
-
             </div>
+            <div className={classes.linesArea}>
+                <VerticalLine className={classes.leftLine}/>
+                <VerticalLine className={classes.rightLine}/>
+            </div>
+            <div className={classes.circleArea}>
+                <div className={classes.circle}>
+                    <span className={classes.circleTitle}>Очищение</span>
+                    <ArrowDown/>
+                    <span className={classes.circleSubTitle}>механический фильтр, угольный <br/>и обеззараживание ультрафиолетом</span>
+                </div>
+                <div className={classes.circle}>
+                    <span className={classes.circleTitle}>Фильтрация</span>
+                    <ArrowDown/>
+                    <span className={classes.circleSubTitle}>не используется система <br/>обратного осмоса</span>
+                </div>
+            </div>
+            <span className={classes.subTitle}>
+                    При производстве используется современное <br/>
+                    оборудование, а также автоматизированная <span className={classes.underLine}>система</span> <br/>
+                   <span className={classes.underLine}>контроля всех технологических этапов</span> производства.
+                </span>
         </div>
     );
 }
