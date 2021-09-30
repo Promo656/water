9ea481397845e471a513647e3bc1921a15eee831
egor <rank2007@mail.ru>
2021-09-30 23:09:25 +0300

    commit

--- src/layout/footer/footer.tsx
+++ src/layout/footer/footer.tsx
@@ -6,11 +6,13 @@ import classNames from "classnames";
 
 const useStyles = makeStyles((theme) => ({
         footer: {
-            height: 221,
             backgroundColor: "#FBFBFB",
             display: "flex",
             alignItems: "center",
             justifyContent: "center"
+        },
+        footer_web: {
+            height: 221
         },
         footerWrap: {
             display: "flex",
@@ -26,6 +28,12 @@ const useStyles = makeStyles((theme) => ({
         infoArea: {
             display: "flex",
             flexDirection: "column",
+        },
+        infoArea_mobile: {
+            order: 1,
+            marginBottom: 40
+        },
+        infoArea_web: {
             marginRight: 85
         },
         text: {
@@ -36,9 +44,16 @@ const useStyles = makeStyles((theme) => ({
         linksArea: {
             display: "flex",
             flexDirection: "column",
+        },
+        linksArea_mobile: {
+            width: "100%",
+            marginBottom: 20
+        },
+        linksArea_web: {
             marginRight: 126
         },
         button: {
+            order: 1,
             width: 209,
             height: 56,
             display: "flex",
@@ -53,6 +68,9 @@ const useStyles = makeStyles((theme) => ({
                 background: "#FBFBFB",
             }
         },
+        button_mobile: {
+            marginBottom: 40
+        },
         reRoot: {
             textTransform: "none"
         }
@@ -64,25 +82,41 @@ function Footer() {
     const classes = useStyles()
     const minMatches = useMediaQuery('(min-width:376px)');
     const maxMatches = useMediaQuery('(max-width:376px)');
+    const footer = classNames(classes.footer, {
+        [classes.footer_web]: minMatches
+    })
     const footerWrap = classNames(classes.footerWrap, {
         [classes.footerWrap_mobile]: maxMatches
     })
+    const infoArea = classNames(classes.infoArea, {
+        [classes.infoArea_mobile]: maxMatches,
+        [classes.infoArea_web]: minMatches
+    })
+    const linksArea = classNames(classes.linksArea, {
+        [classes.linksArea_mobile]: maxMatches,
+        [classes.linksArea_web]: minMatches
+    })
+    const button = classNames(classes.button, {
+        [classes.button_mobile]: maxMatches
+    })
     return (
-        <div className={classes.footer}>
+        <div className={footer}>
             <div className={footerWrap}>
                 {minMatches && <FooterIcon className={classes.footerIcon}/>}
-                <div className={classes.infoArea}>
+                <div className={infoArea}>
                     <span className={classes.text} style={{marginBottom: 17}}>Email: 1@69paralel.ru</span>
                     <span className={classes.text} style={{marginBottom: 17}}>Телефон: +7(8152)65-25-50</span>
-                    <span className={classes.text}>Адрес: Мурманская область, <br/> Кольский район,поселок Пушной</span>
+                    <span className={classes.text} style={{marginBottom: 20}}>Адрес: Мурманская область, <br/> Кольский район,поселок Пушной</span>
+                    {maxMatches && <div style={{border: "1px solid #075AB4", width: "100%"}}></div>}
                 </div>
-                <div className={classes.linksArea}>
-                    <span className={classes.text} style={{marginBottom: 20}}>О компании</span>
-                    <span className={classes.text} style={{marginBottom: 20}}>Партнеры</span>
-                    <span className={classes.text}>Производство</span>
+                <div className={linksArea}>
+                    <div className={classes.text} style={{marginBottom: 20}}>О компании</div>
+                    <div className={classes.text} style={{marginBottom: 20}}>Партнеры</div>
+                    <div className={classes.text} style={{marginBottom: 20}}>Производство</div>
+                    {maxMatches && <div style={{border: "1px solid #075AB4", width: "100%"}}></div>}
                 </div>
                 <Button
-                    className={classes.button}
+                    className={button}
                     classes={{root: classes.reRoot}}
                 >
                     Заказать звонок
