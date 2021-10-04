b148477d89e773379c57939ae058c73860a3332d
egor <rank2007@mail.ru>
2021-10-04 23:11:20 +0300

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "./assets/font/inglobal.ttf";
-import "./assets/font/yrsa.ttf";
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
